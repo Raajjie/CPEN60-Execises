8384f9a2996fc1c1dcc793278d125715f946cbad
dannlineses <113866604+dannlineses@users.noreply.github.com>
2023-04-27 12:40:26 +0800

    Delete Chapter 2 directory

--- Chapter 2/2.12.py
+++
@@ -1,5 +0,0 @@
-height = int(input("Enter height of triangle: "))
-
-for i in range(1, height+1):
-    print('*' * i)
-    
--- Chapter 2/2.13.py
+++
@@ -1,4 +0,0 @@
-height = int(input("Enter height of triangle: "))
-
-for i in range(height, 0, -1):
-    print('*' * i)
