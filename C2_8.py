45fd8fdf0f9db2845ca36cb56386f211cf19587c
JohnceenPulido <113906466+JohnceenPulido@users.noreply.github.com>
2023-04-27 12:42:44 +0800

    Delete C2_8.py

--- C2_8.py
+++
@@ -1,8 +0,0 @@
-#Write a program that asks the user for their name and how many times to print it.
-# The program should print out the user’s name the specified number of times.
-
-name = input("What is your name? ")
-times = int(input("How many times do you want to print it? "))
-
-for i in range(times):
-    print(name)
