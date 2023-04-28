f9a5b7c835565fdc5be54195f3e22047c91af50c
Vin Maghirang <113866728+VinMaghirang@users.noreply.github.com>
2023-04-28 11:56:06 +0800

    Add files via upload

---
+++ Chapter 2/2.2.py
@@ -0,0 +1,14 @@
+#2. Write a program to fill the screen horizontally and vertically with your name. [Hint: add the option end='' into the print function to fill the screen horizontally.]
+
+
+# Get name from user
+name = input("Enter your name: ")
+
+rows = 100
+columns = 1
+
+# Fill screen with name
+for i in range(rows):
+    for j in range(columns):
+        print(name, end='')
+    print()
